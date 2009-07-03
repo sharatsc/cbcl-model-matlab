a0ff44bb3a4397d6b6a9476fd17814af25e41a7f
sharat.chikkerur <sharat.chikkerur@d82b09eb-1d4d-0410-a7ff-59ca0fd5a898>
2009-07-03 17:44:29 +0000

    made sigma adaptive to patch

--- trunk/cbcl-model-matlab/s_grbf.m
+++ trunk/cbcl-model-matlab/s_grbf.m
@@ -3,14 +3,21 @@
 %
 %sharat@mit.edu
 %---------------------------------------------------------
-function [s] = s_generic_tuning(c,patches,sigma)
+function [s] = s_grbf(c,patches,sigma)
 DEBUG            = 0;
 num_bands        = length(c);
 s                = cell(num_bands,1);
 if(nargin<3)
-    sigma        = 1;
+  for p=1:length(patches)
+	sigma(p)=norm(patches{p}(:))/sqrt(6);
+  end;
 end;
 
+if(isscalar(sigma))
+    sigma        = sigma*ones(length(patches),1);
+end;
+
+try
 for b = 1:num_bands
   c_tmp          = c{b};
   [cht,cwt,cdir] = size(c_tmp);
@@ -18,22 +25,25 @@ for b = 1:num_bands
   for p = 1:length(patches)
     patch            = patches{p};
     [pht,pwt,pdir]   = size(patch);
-    if(pht> cht | pwt>cwt) 
-     s{b}=zeros(1,1,cdir);
-     continue;
-    end;%patch larger than image!
+    %if(pht>= cht | pwt>=cwt) 
+    % s{b}=zeros(1,1,cdir);
+    % continue;
+    %end;%patch larger than image!
     ptch2            = sum(patch(:).^2);
-    img2             = conv2(ones(pht,1),ones(pwt,1),sum(c_tmp.^2,3),'valid');
+    img2             = conv2(ones(pht,1),ones(pwt,1),sum(c_tmp.^2,3),'same');
     res_tmp          = img2+ptch2;
     for d     = 1:pdir
       img      = single(c_tmp(:,:,d));
-      tmp      = conv2(img,patch(end:-1:1,end:-1:1,d),'valid');
+      tmp      = conv2(img,patch(end:-1:1,end:-1:1,d),'same');
       res_tmp  = res_tmp-2*tmp;
     end;
-    res_tmp    = exp(-res_tmp/(2*sigma*sigma));
-    res_tmp    = padarray(res_tmp,[floor((pht-1)/2),floor((pwt-1)/2)],'pre');
-    res_tmp    = padarray(res_tmp,[ceil((pht-1)/2),ceil((pwt-1)/2)],'post');
+    res_tmp    = exp(-res_tmp/(2*sigma(p)*sigma(p)));
+    %res_tmp    = padarray(res_tmp,[floor((pht-1)/2),floor((pwt-1)/2)],'pre');
+    %res_tmp    = padarray(res_tmp,[ceil((pht-1)/2),ceil((pwt-1)/2)],'post');
     s{b}(:,:,p)= res_tmp;
-  end;
+  end;%p
 end;
-
+catch
+  err=lasterror;
+  keyboard;
+end;
